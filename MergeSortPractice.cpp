4aa8f5927f2c0a7eede31cc2ab9092197181e5dc
Kian <kian.melhus@gmail.com>
2019-04-06 00:17:51 -0500

    filled in the shell of merge sort still need to seperate funtion definitions from their headers. Mergesort not functioning I'm guessing off by one error somewhere.

--- MergeSortPractice.cpp
+++ MergeSortPractice.cpp
@@ -7,14 +7,56 @@
 #include <vector>
 #include <cstdlib>
 
+
+void merge(std::vector<int>& a, std::vector<int>& aux, int lo, int mid, int hi){
+	int i = lo;
+	int j = mid+1;
+
+	for (int k = lo; k<=hi; k++){
+		aux[k] = a[k];
+	}
+
+	for (int k = lo; k<=hi; k++){
+		if (i > mid){
+			a[k] = aux[j];
+			j++;
+		}
+		else if (j>hi){
+			a[k] = aux[i];
+			i++;
+		}
+		else if (a[i]<a[j]){
+			a[k] = a[i];
+			i++;
+		}
+		else{
+			a[k] = a[j];
+			j++;
+		}
+	}
+}
+
+
+void mergeSort(std::vector<int>& a, std::vector<int>& aux, int lo, int hi){
+	if (hi <= lo)
+		return;
+
+	int mid = lo + (hi - lo)/2;
+	mergeSort(a,aux,lo,mid);
+	mergeSort(a,aux,mid+1,hi);
+	merge(a,aux,lo,mid,hi);
+}
+
 void mergeSort(std::vector<int>& a){
-	
+	int c = a.size();
+	std::vector<int> aux(c);
+	mergeSort(a,aux,0,(a.size() - 1));
 }
 
 int main(){
 	std::vector<int> unsorted;
 
-	for (int i = 0; i<20; i++){
+	for (int i = 0; i<10; i++){
 		unsorted.push_back(rand() % 100);
 	}
 
@@ -23,6 +65,8 @@ int main(){
 	}
 
 	mergeSort(unsorted);
+	
+	std::cout << std::endl << "Displaying sorted array \n";
 
 	for (auto i : unsorted){
 		std::cout << "[" << i << "]";

8cb972718f950ba94de148fb705d9c60c631c6b1
Kian <kian.melhus@gmail.com>
2019-04-06 23:09:10 -0500

    completed quicksort practice trace need to fix the projects modularity and seperate functions from the main driver

--- QuickSortPractice.cpp
+++ QuickSortPractice.cpp
@@ -4,10 +4,16 @@
 //tracing algorithm that uses single partition value for whatever values in index
 //low of the subarray everytime
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <cstdlib>
 
+void quickSort(std::vector<int>& a);
+
+void quickSort(std::vector<int>& a, int lo, int hi);
+
+int partition(std::vector<int>& a, int lo, int hi);
 
 int main(){
 	std::vector<int> unsorted;
@@ -33,3 +39,54 @@ int main(){
 	std::cout << std::endl;
 
 }
+
+void quickSort(std::vector<int>& a){
+	quickSort(a,0,a.size() - 1);
+}
+
+void quickSort(std::vector<int>& a, int lo, int hi){
+	if (hi <= lo){
+		return;
+	}
+	int j = partition(a,lo,hi);
+	quickSort(a,lo,j-1);
+	quickSort(a,j+1,hi);
+}
+
+int partition(std::vector<int>& a, int lo, int hi){
+	int i = lo;
+	int j = hi + 1;
+	int hold = a[lo];
+
+	while (true){
+		while (a[++i]<hold){
+			if (i == hi)	break;
+		}
+		
+		while (hold<a[--j]){
+			if (j == lo)	break;
+		}
+
+		if (i>=j)  break;
+
+		std::swap(a[i],a[j]);
+	}
+
+	std::swap(a[lo],a[j]);
+	return j;
+}
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
